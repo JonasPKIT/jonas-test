055a4718569b4d35956bf72b3e2495ba0acb9953
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-06-09 10:49:37 +0200

    Rename

--- build/failOnFirstBuild.sh
+++ build/failOnFirstBuild.sh
@@ -2,7 +2,7 @@
 
 echo "${GITHUB_REPOSITORY}"
 echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/kithugs" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/kithugs" ]; then
+if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/jonas-test" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/jonas-test" ]; then
   echo "Please run setup.sh REPOSITORY_NAME"
   exit 1
 fi
